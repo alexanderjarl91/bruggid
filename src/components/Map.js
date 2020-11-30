e75937323a0d649d3d22ceee042cc5aa6ba2f1c7
Björg <bjorggunnarsd@gmail.com>
2020-11-30 15:56:47 +0000

    Added map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from "leaflet";
+import beer from "../img/beer.svg";
 
 function Map() {
+    // Access token, style id and user name set in .env variable here for now. Need to find 
+    // another way later because this .env file will be included in the build and will not stay hidden.
+    const mapboxUrl = `https://api.mapbox.com/styles/v1/${process.env.REACT_APP_USER_NAME}/${process.env.REACT_APP_STYLE_ID}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_ACCESS_TOKEN}`
+
+    // Icon constructor for map marker
+    const beerIcon = new Icon({
+        iconUrl: beer,
+        iconSize: [55, 55]
+    });
+
+
     return (
-        <div>
-           MAP COMPONENT
-        </div>
+    <div id='mapid'>
+        <MapContainer center={[65.94419333158635, -18.356791755646736]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>'
+                url= {mapboxUrl}
+                
+            />
+            <Marker position={[65.94419333158635, -18.356791755646736]} icon={beerIcon}>
+                <Popup>
+                    <strong>Bruggsmiðjan Kaldi Brewery</strong> <br /> Öldugata 22, 621 Árskógssandur
+                </Popup>
+            </Marker>
+        </MapContainer>
+    </div>
     )
 }
 
