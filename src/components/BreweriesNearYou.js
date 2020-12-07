adacbe9fe018bbbb90b0f0739f1f3e6457a3ab28
Björg <bjorggunnarsd@gmail.com>
2020-12-07 12:03:27 +0000

    Cleaned up in BNY code

--- src/components/BreweriesNearYou.js
+++ src/components/BreweriesNearYou.js
@@ -3,38 +3,20 @@ import * as geolib from 'geolib';
 
 
 function BreweriesNearYou({breweries, position}) {
-    // const [position, setPosition] = useState(null)
     const [nearestBreweries, setNearestBreweries] = useState()
-
-    // useEffect(() => {
-    //     navigator.geolocation.getCurrentPosition(
-    //         function (response) { 
-    //             setPosition({latitude: response.coords.latitude, longitude: response.coords.longitude})},
-    //        function (error) { console.error(error)},
-    //         {timeout: 1000*60, enableHighAccuracy: true, maximumAge: 1000*60*60}, 
-    //     ); 
-       
-    // }, [])
-
-    // console.log(position)
     
     useEffect(() => {
         console.log(breweries)
         if (!position || breweries.length === 0) return
-         // This needs conditional on position ...
         const nearest = breweries.map((brewery) => {
             const breweryCoord = ({latitude: brewery.latitude, longitude: brewery.longitude}) 
             return {name: brewery.name, breweryCoord, dist: geolib.getDistance(position, breweryCoord)}
         })
         .sort( (a, b) => a.dist - b.dist )
         const copyOfNearest = nearest.slice(0, 5)
-        console.log(position)
-        console.log(copyOfNearest)
 
         setNearestBreweries(copyOfNearest)
 
-        // console.log(nearest)
-        // console.log(nearestBreweries)
     }, [position, breweries])
    
 
@@ -42,8 +24,6 @@ function BreweriesNearYou({breweries, position}) {
        const distance = geolib.convertDistance(originalDistance, 'km')
        return Math.round(distance * 10) / 10
     }
-
-    // convertDistance returns to many keys ... check that out
 
     return (
         <>
